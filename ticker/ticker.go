ca9568a72404909a9dc1dbb3e32e9a81ac4b1ec4
mats <moskjaer@stud.ntnu.no>
2018-10-22 21:25:10 +0200

    Added ticker handler og unit tests.

--- ticker/ticker.go
+++ ticker/ticker.go
@@ -6,9 +6,12 @@
 package ticker
 
 import (
+	"encoding/json"
+	"fmt"
+	"log"
 	"net/http"
-	"sort"
 	"strconv"
+	"time"
 
 	"github.com/mats93/paragliding/mongodb"
 )
@@ -17,30 +20,16 @@ import (
 const CAP = 5
 
 // Timestamp struct.
-var Timestamp struct {
-	TimeLatest int64           `json:"t_latest"`
-	TimeStart  int64           `json:"t_start"`
-	TimeStop   int64           `json:"t_stop"`
-	Tracks     map[int64]int64 `json:"tracks"`
-	Processing int64           `json:"processing"`
+type Timestamp struct {
+	TimeLatest int64         `json:"t_latest"`
+	TimeStart  int64         `json:"t_start"`
+	TimeStop   int64         `json:"t_stop"`
+	Tracks     []int         `json:"tracks"`
+	Processing time.Duration `json:"processing"`
 }
 
 // Collection is the MongoDB collection to use. Gets injected from main or test.
 var Collection string
-
-// Takes a slice of Tracks, sorts them from newest to oldest (increasing), returns the sortet slice.
-func sortTrackByTimestamp(track []mongodb.Track) []mongodb.Track {
-	// The function works on a buffer.
-	buffer := append([]mongodb.Track(nil), track...)
-
-	// Sorts the slice based on the Timestmap.
-	sort.Slice(buffer, func(i, j int) bool {
-		return buffer[i].Timestamp > buffer[j].Timestamp
-	})
-
-	// Returns the sorted track.
-	return buffer
-}
 
 // GetLastTimestamp - GET: Returns the timestamp of the last added track.
 // Output: text/plain
@@ -63,7 +52,7 @@ func GetLastTimestamp(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
 
 		// Sorts the track from high to low.
-		sortedTracks := sortTrackByTimestamp(tracks)
+		sortedTracks := mongodb.SortTrackByTimestamp(tracks)
 
 		// Converts int64 to string.
 		timestamp := strconv.FormatInt(sortedTracks[0].Timestamp, 10)
@@ -76,11 +65,138 @@ func GetLastTimestamp(w http.ResponseWriter, r *http.Request) {
 // GetTimestamps - GET: Returns timestamp information.
 // Output: application/json
 func GetTimestamps(w http.ResponseWriter, r *http.Request) {
+	// Start time of the request.
+	start := time.Now()
 
+	// Connects to the database.
+	database := mongodb.DatabaseInit(Collection)
+
+	// Gets all tracks from the DB.
+	tracks, err := database.FindAll()
+	if err != nil || tracks == nil {
+		// Sets header content-type to application/json and status code to 204 (No content).
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusNoContent)
+
+		// Outputs error.
+		w.Write([]byte("There are no tracks in the database"))
+	} else {
+		var newTimestamp Timestamp
+
+		// Lengt of the track slice (-1 = last index used).
+		len := len(tracks) - 1
+
+		// Last added timestamp (Last index in the DB).
+		newTimestamp.TimeLatest = tracks[len].Timestamp
+
+		// Sorts the tracks by timestamps, last added in index 0, first added in last index.
+		sortedTracks := mongodb.SortTrackByTimestamp(tracks)
+
+		// Adds timestamps to Timestamp struct.
+		newTimestamp.TimeStart = sortedTracks[0].Timestamp
+		newTimestamp.TimeStop = sortedTracks[len].Timestamp
+
+		// The max allowed IDs to be returned.
+		var maxLoops int
+		if len+1 > CAP {
+			maxLoops = CAP - 1
+		} else {
+			maxLoops = len
+		}
+
+		// Adds the IDs to the slice.
+		for i := 0; i <= maxLoops; i++ {
+			newTimestamp.Tracks = append(newTimestamp.Tracks, tracks[i].ID)
+		}
+
+		// Adds the processing time.
+		newTimestamp.Processing = time.Since(start)
+
+		// Converts the struct to json.
+		json, err := json.Marshal(newTimestamp)
+		if err != nil {
+			// Sets header status code to 500 "Internal server error" and logs the error.
+			w.WriteHeader(http.StatusInternalServerError)
+			log.Fatal(err)
+		} else {
+			// Sets header content-type to application/json and status code to 200 (OK).
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusOK)
+
+			// Returns API info as json.
+			w.Write([]byte(json))
+		}
+	}
 }
 
 // GetTimestampsNewerThen - GET: Returns timestamp information of newer timestamps then provided.
 // Output: application/json
 func GetTimestampsNewerThen(w http.ResponseWriter, r *http.Request) {
+	// Start time of the request.
+	start := time.Now()
 
+	var ts int64
+
+	// Gets the timestamp from the URL.
+	fmt.Sscanf(r.URL.Path, "/paragliding/api/ticker/%d", &ts)
+
+	// Connects to the database.
+	database := mongodb.DatabaseInit(Collection)
+
+	// Gets all tracks from the DB.
+	tracks, err := database.FindTrackHigherThen(ts)
+	if err != nil || tracks == nil {
+		// Sets header content-type to application/json and status code to 204 (No content).
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusNoContent)
+
+		// Outputs error.
+		w.Write([]byte("There are no tracks in the database"))
+	} else {
+		var newTimestamp Timestamp
+
+		// Lengt of the track slice (-1 = last index used).
+		len := len(tracks) - 1
+
+		// Last added timestamp (Last index in the DB).
+		newTimestamp.TimeLatest = tracks[len].Timestamp
+
+		// Sorts the tracks by timestamps, last added in index 0, first added in last index.
+		sortedTracks := mongodb.SortTrackByTimestamp(tracks)
+
+		// Adds timestamps to Timestamp struct.
+		newTimestamp.TimeStart = sortedTracks[0].Timestamp
+		newTimestamp.TimeStop = sortedTracks[len].Timestamp
+
+		// The max allowed IDs to be returned.
+		var maxLoops int
+		if len+1 > CAP {
+			maxLoops = CAP - 1
+		} else {
+			maxLoops = len
+		}
+
+		// Adds the IDs to the slice.
+		for i := 0; i <= maxLoops; i++ {
+			newTimestamp.Tracks = append(newTimestamp.Tracks, tracks[i].ID)
+		}
+
+		// Adds the processing time.
+		newTimestamp.Processing = time.Since(start)
+
+		// Converts the struct to json.
+		json, err := json.Marshal(newTimestamp)
+		if err != nil {
+			// Sets header status code to 500 "Internal server error" and logs the error.
+			w.WriteHeader(http.StatusInternalServerError)
+			log.Fatal(err)
+		} else {
+			// Sets header content-type to application/json and status code to 200 (OK).
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusOK)
+
+			// Returns API info as json.
+			w.Write([]byte(json))
+		}
+	}
 }
--- ticker/ticker_test.go
+++ ticker/ticker_test.go
@@ -8,41 +8,13 @@ package ticker
 import (
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 	"time"
 
 	"github.com/gorilla/mux"
 	"github.com/mats93/paragliding/mongodb"
 )
-
-// Function to test: sortTrackByTimestamp().
-// Test to check if the slice was sorted correctly.
-func Test_sortTrackByTimestamp(t *testing.T) {
-	// Injects the MongoDB collection to use.
-	Collection = "Tests"
-
-	// Connects the the database and inserts 3 tracks.
-	// The last inserted has the highest timestamp.
-	database := mongodb.DatabaseInit(Collection)
-	database.Insert(mongodb.Track{1, 111, time.Now(), "pilot1", "glider1", "glider_id1", 20.1, "http://test1.test"})
-	database.Insert(mongodb.Track{2, 222, time.Now(), "pilot2", "glider2", "glider_id2", 20.2, "http://test2.test"})
-	database.Insert(mongodb.Track{3, 333, time.Now(), "pilot3", "glider3", "glider_id3", 20.3, "http://test3.test"})
-
-	// Returns all tracks from the DB.
-	tracks, _ := database.FindAll()
-
-	// Try to sort the slice.
-	sorted := sortTrackByTimestamp(tracks)
-
-	// The unsorted track should have the highest timestamp in index 2.
-	// The sorted track should have the highest timestamp in index 0.
-	if sorted[2].Timestamp > sorted[0].Timestamp {
-		t.Errorf("Function did not sort correctly")
-	}
-
-	// Removes the test data.
-	database.DeleteAllTracks()
-}
 
 // Function to test: GetLastTimestamp().
 // Test to check the returned status code, content-type and data for the function, when the DB is empty.
@@ -124,12 +96,243 @@ func Test_GetLastTimestamp(t *testing.T) {
 
 	// Expected timestamp to be returned.
 	expected := "333"
+
 	// The actual retuned data.
 	actual := recorder.Body.String()
 
 	if actual != expected {
 		t.Errorf("Handler returned wrong data: got %s want %s",
 			actual, expected)
+	}
+
+	// Removes the test data.
+	database.DeleteAllTracks()
+}
+
+// Function to test: GetLastTimestamp().
+// Test to check the returned status code, content-type and data for the function, when the DB is empty.
+func Test_GetTimestamps_Empty(t *testing.T) {
+	// Injects the MongoDB collection to use.
+	Collection = "Tests"
+
+	// Creates a request that is passed to the handler.
+	request, _ := http.NewRequest("GET", "/paragliding/api/ticker/", nil)
+
+	// Creates the recorder and router.
+	recorder := httptest.NewRecorder()
+	router := mux.NewRouter()
+
+	// Tests the function.
+	router.HandleFunc("/paragliding/api/ticker/", GetTimestamps).Methods("GET")
+	router.ServeHTTP(recorder, request)
+
+	// Check the status code is what we expect (204).
+	status := recorder.Code
+	if status != http.StatusNoContent {
+		t.Errorf("Handler returned wrong status code: got %v want %v",
+			status, http.StatusNoContent)
+	}
+
+	// Check if the content-type is what we expect (application/json).
+	content := recorder.HeaderMap.Get("content-type")
+	if content != "application/json" {
+		t.Errorf("Handler returned wrong content-type: got %s want %s",
+			content, "application/json")
+	}
+
+	// Expected error message.
+	expected := "There are no tracks in the database"
+	// The actual error message returned.
+	actual := recorder.Body.String()
+
+	if actual != expected {
+		t.Errorf("Handler returned wrong data: got %s want %s",
+			actual, expected)
+	}
+}
+
+// Function to test: GetLastTimestamp().
+// Test to check the returned status code, content-type and data for the function.
+func Test_GetTimestamps(t *testing.T) {
+	// Injects the MongoDB collection to use.
+	Collection = "Tests"
+
+	// Connects the the database and inserts 3 tracks.
+	database := mongodb.DatabaseInit(Collection)
+	database.Insert(mongodb.Track{1, 111, time.Now(), "pilot1", "glider1", "glider_id1", 20.1, "http://test1.test"})
+	database.Insert(mongodb.Track{2, 222, time.Now(), "pilot2", "glider2", "glider_id2", 20.2, "http://test2.test"})
+	database.Insert(mongodb.Track{3, 333, time.Now(), "pilot3", "glider3", "glider_id3", 20.3, "http://test3.test"})
+
+	// Creates a request that is passed to the handler.
+	request, _ := http.NewRequest("GET", "/paragliding/api/ticker/", nil)
+
+	// Creates the recorder and router.
+	recorder := httptest.NewRecorder()
+	router := mux.NewRouter()
+
+	// Tests the function.
+	router.HandleFunc("/paragliding/api/ticker/", GetTimestamps).Methods("GET")
+	router.ServeHTTP(recorder, request)
+
+	// Check the status code is what we expect (200).
+	status := recorder.Code
+	if status != http.StatusOK {
+		t.Errorf("Handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+
+	// Check if the content-type is what we expect (application/json).
+	content := recorder.HeaderMap.Get("content-type")
+	if content != "application/json" {
+		t.Errorf("Handler returned wrong content-type: got %s want %s",
+			content, "application/json")
+	}
+
+	// Expected timestamp to be returned.
+	expectedLatestJson := "{\"t_latest\":333"
+	expectedStartJson := "\"t_start\":333"
+	expectedStopJson := "\"t_stop\":111"
+	expectedIDsJson := "\"tracks\":[1"
+
+	// The actual retuned data.
+	// Dont want to compare against processing. Splits the json and compers with it.
+	actual := recorder.Body.String()
+	actualSplit := strings.Split(actual, ",")
+	actualLatestJson := actualSplit[0]
+	actualStartJson := actualSplit[1]
+	actualStopJson := actualSplit[2]
+	actualIDsJson := actualSplit[3]
+
+	if actualLatestJson != expectedLatestJson {
+		t.Errorf("Function returned wrong Latest field: got %s want %s",
+			actualLatestJson, expectedLatestJson)
+	}
+	if actualStartJson != expectedStartJson {
+		t.Errorf("Function returned wrong Start field: got %s want %s",
+			actualStartJson, expectedStartJson)
+	}
+	if actualStopJson != expectedStopJson {
+		t.Errorf("Function returned wrong Stop field: got %s want %s",
+			actualStopJson, expectedStopJson)
+	}
+	if actualIDsJson != expectedIDsJson {
+		t.Errorf("Function returned wrong IDs field: got %s want %s",
+			actualIDsJson, expectedIDsJson)
+	}
+
+	// Removes the test data.
+	database.DeleteAllTracks()
+}
+
+// Function to test: GetTimestampsNewerThen().
+// Test to check the returned status code, content-type and data for the function, when the DB is empty.
+func Test_GetTimestampsNewerThen_Empty(t *testing.T) {
+	// Injects the MongoDB collection to use.
+	Collection = "Tests"
+
+	// Creates a request that is passed to the handler.
+	request, _ := http.NewRequest("GET", "/paragliding/api/ticker/1", nil)
+
+	// Creates the recorder and router.
+	recorder := httptest.NewRecorder()
+	router := mux.NewRouter()
+
+	// Tests the function.
+	router.HandleFunc("/paragliding/api/ticker/1", GetTimestampsNewerThen).Methods("GET")
+	router.ServeHTTP(recorder, request)
+
+	// Check the status code is what we expect (204).
+	status := recorder.Code
+	if status != http.StatusNoContent {
+		t.Errorf("Handler returned wrong status code: got %v want %v",
+			status, http.StatusNoContent)
+	}
+
+	// Check if the content-type is what we expect (application/json).
+	content := recorder.HeaderMap.Get("content-type")
+	if content != "application/json" {
+		t.Errorf("Handler returned wrong content-type: got %s want %s",
+			content, "application/json")
+	}
+
+	// Expected error message.
+	expected := "There are no tracks in the database"
+	// The actual error message returned.
+	actual := recorder.Body.String()
+
+	if actual != expected {
+		t.Errorf("Handler returned wrong data: got %s want %s",
+			actual, expected)
+	}
+}
+
+// Function to test: GetTimestampsNewerThen().
+// Test to check the returned status code, content-type and data for the function.
+func Test_GetTimestampsNewerThen(t *testing.T) {
+	// Injects the MongoDB collection to use.
+	Collection = "Tests"
+
+	// Connects the the database and inserts 3 tracks.
+	database := mongodb.DatabaseInit(Collection)
+	database.Insert(mongodb.Track{1, 111, time.Now(), "pilot1", "glider1", "glider_id1", 20.1, "http://test1.test"})
+	database.Insert(mongodb.Track{2, 222, time.Now(), "pilot2", "glider2", "glider_id2", 20.2, "http://test2.test"})
+	database.Insert(mongodb.Track{3, 333, time.Now(), "pilot3", "glider3", "glider_id3", 20.3, "http://test3.test"})
+
+	// Creates a request that is passed to the handler.
+	request, _ := http.NewRequest("GET", "/paragliding/api/ticker/222", nil)
+
+	// Creates the recorder and router.
+	recorder := httptest.NewRecorder()
+	router := mux.NewRouter()
+
+	// Tests the function.
+	router.HandleFunc("/paragliding/api/ticker/222", GetTimestampsNewerThen).Methods("GET")
+	router.ServeHTTP(recorder, request)
+
+	// Check the status code is what we expect (200).
+	status := recorder.Code
+	if status != http.StatusOK {
+		t.Errorf("Handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+
+	// Check if the content-type is what we expect (application/json).
+	content := recorder.HeaderMap.Get("content-type")
+	if content != "application/json" {
+		t.Errorf("Handler returned wrong content-type: got %s want %s",
+			content, "application/json")
+	}
+
+	// Expected timestamp to be returned.
+	expectedLatestJson := "{\"t_latest\":333"
+	expectedStartJson := "\"t_start\":333"
+	expectedStopJson := "\"t_stop\":333"
+	expectedIDsJson := "\"tracks\":[3]"
+
+	// The actual retuned data.
+	// Dont want to compare against processing. Splits the json and compers with it.
+	actual := recorder.Body.String()
+	actualSplit := strings.Split(actual, ",")
+	actualLatestJson := actualSplit[0]
+	actualStartJson := actualSplit[1]
+	actualStopJson := actualSplit[2]
+	actualIDsJson := actualSplit[3]
+
+	if actualLatestJson != expectedLatestJson {
+		t.Errorf("Function returned wrong Latest field: got %s want %s",
+			actualLatestJson, expectedLatestJson)
+	}
+	if actualStartJson != expectedStartJson {
+		t.Errorf("Function returned wrong Start field: got %s want %s",
+			actualStartJson, expectedStartJson)
+	}
+	if actualStopJson != expectedStopJson {
+		t.Errorf("Function returned wrong Stop field: got %s want %s",
+			actualStopJson, expectedStopJson)
+	}
+	if actualIDsJson != expectedIDsJson {
+		t.Errorf("Function returned wrong IDs field: got %s want %s",
+			actualIDsJson, expectedIDsJson)
 	}
 
 	// Removes the test data.
